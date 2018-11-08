79aa82fc95b2cfc790d8ab5f3c48c521d8735020
zhaojizhen <1299579097@qq.com>
2018-11-08 19:37:42 +0800

    2018/11/8 -19:37

--- src/components/wode/Wode.vue
+++ src/components/wode/Wode.vue
@@ -11,15 +11,49 @@
                         <p class="upho" >177****4562</p>
                   </div>  
                 </div>
-            <div>
-                    <ul class="huyu">
-                        <li class="huyuka"><p>¥0</p> <span>会员卡</span></li>
-                        <li class="huyukb"><p>1</p> <span>优惠券</span></li>
-                        <li class="huyukc"><p>62</p> <span>积分</span></li>
-                    </ul>
-            </div>
-          </div>
+                <div>
+                    <div>
+                            <ul class="huyu">
+                                <li class="huyuka"><p>¥0</p> <span>会员卡</span></li>
+                                <li class="huyukb"><p>1</p> <span>优惠券</span></li>
+                                <li class="huyukc"><p>62</p> <span>积分</span></li>
+                            </ul>
+                    </div>
+                  
+                    </div>
+                </div> 
       </div>
+      <div class="twose">
+         <span>生鲜订单</span>
+         <span>全部</span>
+         <i class="iconfont icon-right twoseb"></i>
+      </div>
+        <div class="shenga">
+                        <ul class="shengxiandingdan">
+                            <li class="items"><img src="../../assets/logo.png" alt=""><p>待付款</p></li>
+                            <li class="items"><img src="../../assets/logo.png" alt=""><p>待发货</p></li>
+                            <li class="items"><img src="../../assets/logo.png" alt=""><p>待收货</p></li>
+                            <li class="items"><img src="../../assets/logo.png" alt=""><p>待评价</p></li>
+                            <li class="items"><img src="../../assets/logo.png" alt=""><p>退款/售后</p></li>    
+                        </ul>
+        </div>
+        <div>
+            <p class="tit">常用功能</p>
+            <div>
+                <ul class="tita">
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                    <li class="titb"><img src="../../assets/logo.png" alt=""><p>物业缴纳</p></li>
+                </ul>
+            </div>
+        </div>
+        
+                   
   </div>
 </template>
 <script>
